1c45b6d308e7466d5e4f0aa4383ee616172cd22b
ThePC <ThePC@DESKTOP-3MTRAIS>
2018-09-27 06:50:38 -0400

    hw

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,31 @@
 
-public class BankAccount {
-
+public class BankAccount 
+{
+	private String name;
+	private int accNum;
+	private double balance;
+	
+	public BankAccount()
+	{
+		name = "MatthewTo";
+		accNum = 1719;
+		balance = 0;
+	}
+	public void deposit(double depositAmount)
+	{
+		balance -= depositAmount;
+	}
+	public void withdraw(double withdrawAmount)
+	{
+		balance += withdrawAmount;
+	}
+	public double getBalance()
+	{
+		return balance;
+	}
+	public String toString()
+	{
+		String info = name + accNum + balance;
+		return info;
+	}
 }
